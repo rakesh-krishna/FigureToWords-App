714891f966d735449addcc0a7a86c6e024b98cde
A S Rakesh Krishna <rakeshkrishna517@gmail.com>
2021-01-18 08:01:16 +0530

    added support for  paise

--- app/src/main/java/com/example/figuretowords/MainActivity.java
+++ app/src/main/java/com/example/figuretowords/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.figuretowords;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Collections;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     TextView input;
     TextView output;
     Button button;
@@ -22,49 +30,76 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button.setOnClickListener(this);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public void onClick(View v) {
         String word;
         word=NumberToWord(input.getText().toString());
         if (word.trim().length()==0) {word="Zero only.";}
-        output.setText(word+"only.");
+        output.setText(word);
     }
-    private static String NumberToWord (String number)
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private static String NumberToWord (String num)
     {
-        String twodigitword="";
-        String word="";
-        String[] HTLC = {"", "Hundred", "Thousand", "Lakh", "Crore"};
-        int split[]={0,2, 3, 5, 7,9};
-        String[] temp=new String[split.length];
-        boolean addzero=true;
-        int len1=number.length();
-        if (len1>split[split.length-1]) { System.out.println("Error. Maximum Allowed digits "+ split[split.length-1]);
-            System.exit(0);
-        }
-        for (int l=1 ; l<split.length; l++ )
-            if (number.length()==split[l] ) addzero=false;
-        if (addzero==true) number="0"+number;
-        int len=number.length();
-        int j=0;
-        //spliting & putting numbers in temp array.
-        while (split[j]<len)
-        {
-            int beg=len-split[j+1];
-            int end=beg+split[j+1]-split[j];
-            temp[j]=number.substring(beg , end);
-            j=j+1;
+        BigDecimal bd = new BigDecimal(num);
+        long number = bd.longValue();
+        long no = bd.longValue();
+        int decimal = (int) (bd.remainder(BigDecimal.ONE).doubleValue() * 100);
+        int digits_length = String.valueOf(no).length();
+        int i = 0;
+        ArrayList<String> str = new ArrayList<>();
+        HashMap<Integer, String> words = new HashMap<>();
+        words.put(0, "");
+        words.put(1, "One");
+        words.put(2, "Two");
+        words.put(3, "Three");
+        words.put(4, "Four");
+        words.put(5, "Five");
+        words.put(6, "Six");
+        words.put(7, "Seven");
+        words.put(8, "Eight");
+        words.put(9, "Nine");
+        words.put(10, "Ten");
+        words.put(11, "Eleven");
+        words.put(12, "Twelve");
+        words.put(13, "Thirteen");
+        words.put(14, "Fourteen");
+        words.put(15, "Fifteen");
+        words.put(16, "Sixteen");
+        words.put(17, "Seventeen");
+        words.put(18, "Eighteen");
+        words.put(19, "Nineteen");
+        words.put(20, "Twenty");
+        words.put(30, "Thirty");
+        words.put(40, "Forty");
+        words.put(50, "Fifty");
+        words.put(60, "Sixty");
+        words.put(70, "Seventy");
+        words.put(80, "Eighty");
+        words.put(90, "Ninety");
+        String digits[] = {"", "Hundred", "Thousand", "Lakh", "Crore"};
+        while (i < digits_length) {
+            int divider = (i == 2) ? 10 : 100;
+            number = no % divider;
+            no = no / divider;
+            i += divider == 10 ? 1 : 2;
+            if (number > 0) {
+                int counter = str.size();
+                String plural = (counter > 0 && number > 9) ? "s" : "";
+                String tmp = (number < 21) ? words.get(Integer.valueOf((int) number)) + " " + digits[counter] + plural : words.get(Integer.valueOf((int) Math.floor(number / 10) * 10)) + " " + words.get(Integer.valueOf((int) (number % 10))) + " " + digits[counter] + plural;
+                str.add(tmp);
+            } else {
+                str.add("");
+            }
         }
 
-        for (int k=0;k<j;k++)
-        {
-            twodigitword=ConvertOnesTwos(temp[k]);
-            if (k>=1){
-                if (twodigitword.trim().length()!=0) word=twodigitword+" " +HTLC[k] +" "+word;
-            }
-            else word=twodigitword ;
-        }
-        return (word);
+        Collections.reverse(str);
+        String Rupees = String.join(" ", str).trim();
+
+        String paise = (decimal) > 0 ? " And " + words.get(Integer.valueOf((int) (decimal - decimal % 10))) + " " + words.get(Integer.valueOf((int) (decimal % 10))) : "";
+        return  Rupees +" Rupees"+ paise + " paise  Only";
     }
+
 
     private static String ConvertOnesTwos(String t)
     {
